bdb8ffb99dfbb6760aff6e764fa998e16596010a
Artem Malyi <artem.malyi@gmail.com>
2022-04-13 21:48:38 +0300

    more accurate config settings for drv-template

--- make/vcbuild.bat
+++ make/vcbuild.bat
@@ -5,7 +5,7 @@ rem +---------------------------------------------------------------------------
 
 rem Important: if the path contains spaces, escape them with ^. Do not quote the path!
 
-set WDKINCPATH=C:\Program^ Files^ (x86)\Windows^ Kits\10\Include\10.0.19041.0
-set WDKLIBPATH=C:\Program^ Files^ (x86)\Windows^ Kits\10\Lib\10.0.19041.0
+set WDKINCPATH=C:\Program^ Files^ (x86)\Windows^ Kits\10\Include\10.0.22000.0
+set WDKLIBPATH=C:\Program^ Files^ (x86)\Windows^ Kits\10\Lib\10.0.22000.0
 
 win-templates.sln
